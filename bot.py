044995b89f83a84c2b8b36e0c04d6bcdcd92ac65
abdullah626 <89883234+abdullah626@users.noreply.github.com>
2021-11-28 14:35:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 anlik_calisan = []
 
-@client.on(events.NewMessage(pattern='^(?i)/cancel@UtagOnlineBot'))
+@client.on(events.NewMessage(pattern='^(?i)/cancel'))
 async def cancel(event):
   global anlik_calisan
   anlik_calisan.remove(event.chat_id)
